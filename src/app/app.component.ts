82b203507cd332e64c67f91b46259b0bbfb681d0
RenataSentsova <renatasentsova@gmail.com>
2020-05-30 18:54:17 -0400

    add Searching

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,10 @@ import {Satellite} from './info/satellite';
 })
 export class AppComponent {
   sourceList: Satellite[];
+  displayList: Satellite[];
 
   constructor() {
+    this.displayList = [];
     this.sourceList = [];
     const satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
 
@@ -23,8 +25,24 @@ export class AppComponent {
         } else {
           alert('Error: no data');
         }
+        this.displayList = this.sourceList.slice(0);
       }.bind(this));
     }.bind(this));
 
+  }
+
+  search(searchTerm: string): void {
+    const matchingSatellites: Satellite[] = [];
+    searchTerm = searchTerm.toLowerCase();
+    // tslint:disable-next-line:prefer-for-of
+    for (let i = 0; i < this.sourceList.length; i++) {
+      const name = this.sourceList[i].name.toLowerCase();
+      if (name.indexOf(searchTerm) >= 0) {
+        matchingSatellites.push(this.sourceList[i]);
+      }
+    }
+    // assign this.displayList to be the the array of matching satellites
+    // this will cause Angular to re-make the table, but now only containing matches
+    this.displayList = matchingSatellites;
   }
 }
--- src/app/orbit-list/orbit-list.component.ts
+++ src/app/orbit-list/orbit-list.component.ts
@@ -13,4 +13,14 @@ export class OrbitListComponent implements OnInit {
   ngOnInit() {
   }
 
+  sort(column: string): void {
+    this.satellites.sort((a: Satellite, b: Satellite) => {
+      if (a[column] < b[column]) {
+        return -1;
+      } else if (a[column] > b[column]) {
+        return 1;
+      }
+      return 0;
+    });
+  }
 }
