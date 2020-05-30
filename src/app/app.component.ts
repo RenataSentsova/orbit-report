8c9ecfbe34aba351c43bb3ebfd96a471b235ba7e
RenataSentsova <renatasentsova@gmail.com>
2020-05-30 17:23:52 -0400

    Fetch Satellite Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,12 +10,21 @@ export class AppComponent {
   sourceList: Satellite[];
 
   constructor() {
-    this.sourceList = [
-      new Satellite('SiriusXM', 'Communication', '2009-03-21', 'LOW', true),
-      new Satellite('Cat Scanner', 'Imaging', '2012-01-05', 'LOW', true),
-      new Satellite('Weber Grill', 'Space Debris', '1996-03-25', 'HIGH', false),
-      new Satellite('GPS 938', 'Positioning', '2001-11-01', 'HIGH', true),
-      new Satellite('ISS', 'Space Station', '1998-11-20', 'LOW', true),
-    ];
+    this.sourceList = [];
+    const satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+
+    window.fetch(satellitesUrl).then(function(response) {
+      response.json().then(function(data) {
+        const fetchedSatellites = data.satellites;
+        if (fetchedSatellites.length) {
+          fetchedSatellites.forEach(({ name, type, launchDate, orbitType, operational }) => {
+            this.sourceList.push(new Satellite(name, type, launchDate, orbitType, operational));
+          });
+        } else {
+          alert('Error: no data');
+        }
+      }.bind(this));
+    }.bind(this));
+
   }
 }
